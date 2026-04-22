5f0eb01c122ee6a1e9bc2d982a5968c83f35c1cf
dosen <fikri@pcr.ac.id>
2026-04-22 07:11:55 +0700

    Pertemuan 4 : MaterialDialog & Snackbar

--- app/src/main/java/com/example/sonicapps/pertemuan_4/FourthActivity.kt
+++ app/src/main/java/com/example/sonicapps/pertemuan_4/FourthActivity.kt
@@ -10,6 +10,9 @@ import androidx.core.view.WindowInsetsCompat
 import com.example.sonicapps.MainActivity
 import com.example.sonicapps.R
 import com.example.sonicapps.databinding.ActivityFourthBinding
+import com.example.sonicapps.pertemuan_2.SecondActivity
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.snackbar.Snackbar
 
 class FourthActivity : AppCompatActivity() {
     lateinit var binding: ActivityFourthBinding
@@ -35,6 +38,27 @@ class FourthActivity : AppCompatActivity() {
 
         binding.btnBack.setOnClickListener {
             finish()
+        }
+        binding.btnShowSnackbar.setOnClickListener {
+            Snackbar.make(binding.root, "Ini adalah Snackbar", Snackbar.LENGTH_SHORT)
+                .setAction("Tutup"){
+                    Log.e("Info Snackbar","Snackbar ditutup")
+                }
+                .show()
+        }
+        binding.btnShowAlertDialog.setOnClickListener {
+            MaterialAlertDialogBuilder(this)
+                .setTitle("Konfirmasi")
+                .setMessage("Apakah Anda yakin ingin melanjutkan?")
+                .setPositiveButton("Ya") { dialog, _ ->
+                    dialog.dismiss()
+                    Log.e("Info Dialog","Anda memilih Ya!")
+                }
+                .setNegativeButton("Batal") { dialog, _ ->
+                    dialog.dismiss()
+                    Log.e("Info Dialog","Anda memilih Tidak!")
+                }
+                .show()
         }
     }
 
