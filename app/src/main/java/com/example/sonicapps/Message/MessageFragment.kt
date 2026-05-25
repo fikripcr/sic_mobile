fbfb1f34536cd45cfb4ad9bd9ff55bc44b938c9c
dosen <fikri@pcr.ac.id>
2026-05-25 08:47:19 +0700

    Pertemuan 11: Penambahan On Boarding Screen & DotsIndikator

--- app/src/main/java/com/example/sonicapps/Message/MessageFragment.kt
+++ app/src/main/java/com/example/sonicapps/Message/MessageFragment.kt
@@ -1,14 +1,20 @@
 package com.example.sonicapps.Message
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import com.example.sonicapps.Message.tutorial.TutorialMessageActivity
 import com.example.sonicapps.R
 import com.example.sonicapps.databinding.FragmentHomeBinding
 import com.example.sonicapps.databinding.FragmentMessageBinding
+import kotlin.jvm.java
 
 class MessageFragment : Fragment() {
 
@@ -41,8 +47,25 @@ class MessageFragment : Fragment() {
         (requireActivity() as AppCompatActivity).supportActionBar?.apply {
             title = "Message"
         }
+        setHasOptionsMenu(true)
+
         val adapter = MessageAdapter(requireContext(), messageList)
         binding.listMessageItems.adapter = adapter
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.message_toolbar_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_tutorial -> {
+                val intent = Intent(requireContext(), TutorialMessageActivity::class.java)
+                startActivity(intent)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
 }
